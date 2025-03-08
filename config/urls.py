7daa9d060394a66a8460f9ea3a731d29af6e03a3
naybyal <officiallynabiel@gmail.com>
2025-03-08 21:43:17 +0530

    initial commit

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('apps.corrosion.urls')),  # Include the corrosion app's URLs
+    path('api/', include('apps.corrosion.urls')),  
 ]
